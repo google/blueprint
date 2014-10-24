debef53c56262f102b439c28140013cc43fdc650
Jamie Gennis <jgennis@google.com>
2014-10-24 10:42:57 -0700

    Add a Glob func that returns dirs searched
    Change-Id: I35ab9a5f1c7425f42d0e667bac38450afa34c738

---
+++ blueprint/pathtools/glob.go
@@ -0,0 +1,46 @@
+package pathtools
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+// Glob returns the list of files that match the given pattern along with the
+// list of directories that were searched to construct the file list.
+func Glob(pattern string) (matches, dirs []string, err error) {
+	matches, err = filepath.Glob(pattern)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	wildIndices := wildElements(pattern)
+
+	if len(wildIndices) > 0 {
+		for _, match := range matches {
+			dir := filepath.Dir(match)
+			dirElems := strings.Split(dir, string(filepath.Separator))
+
+			for _, index := range wildIndices {
+				dirs = append(dirs, filepath.Join(dirElems[:index]...))
+			}
+		}
+	}
+
+	return
+}
+
+func wildElements(pattern string) []int {
+	elems := strings.Split(pattern, string(filepath.Separator))
+
+	var result []int
+	for i, elem := range elems {
+		if isWild(elem) {
+			result = append(result, i)
+		}
+	}
+	return result
+}
+
+func isWild(pattern string) bool {
+	return strings.ContainsAny(pattern, "*?[")
+}
