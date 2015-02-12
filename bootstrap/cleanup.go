d9d92cb75d40524df388d6e94497ebcf1c15d107
Colin Cross <ccross@android.com>
2015-02-12 10:24:29 -0800

    Print names of files deleted by cleanup phase
    Change-Id: Ia9c5408b1b159843dbc57e2e480ca2972598186d

--- bootstrap/cleanup.go
+++ bootstrap/cleanup.go
@@ -126,6 +126,7 @@ func removeFileAndEmptyDirs(path string) error {
 		}
 		return err
 	}
+	fmt.Printf("removed old ninja-created file %s because it has no rule to generate it\n", path)
 
 	path, err = filepath.Abs(path)
 	if err != nil {
