1c7da229f11bc75499364c7c3c895b0c852d9296
Dan Willemsen <dwillemsen@google.com>
2015-06-10 16:51:16 -0700

    Add flags to bpfind (-name, -without)
    These are useful for the Android build, where we want to identify
    Android.bp files that don't have matching Android.mk files.
    
    Change-Id: I7d8c596f389360c9fbb97e255db78c5c38a500a8

--- bpfind/bpfind.go
+++ bpfind/bpfind.go
@@ -9,6 +9,13 @@ import (
 	"github.com/google/blueprint"
 )
 
+// TODO: implement, need to set a variable in the top-level scope
+// This would let you start from a directory that isn't the root directory
+//var subname = flag.String("subname", "Android.bp", "Default subname")
+
+var name = flag.String("name", "", "Only print blueprints with this file name")
+var without = flag.String("without", "", "Only print blueprints without this file in the same directory")
+
 func main() {
 	flag.Parse()
 
@@ -28,6 +35,15 @@ func main() {
 	}
 
 	for _, file := range blueprints {
+		if *name != "" && filepath.Base(file) != *name {
+			continue
+		}
+		if *without != "" {
+			_, err := os.Stat(filepath.Join(filepath.Dir(file), *without))
+			if !os.IsNotExist(err) {
+				continue
+			}
+		}
 		fmt.Println(file)
 	}
 }
