8c602f7f16ed571918eba4e8e0e0630b3ecd55be
Colin Cross <ccross@android.com>
2015-12-17 18:02:11 -0800

    Add ModuleSubDir to Context and SingletonContext
    Singletons may need ModuleSubDir, for example to implement a stable sort
    on modules.  Add ModuleSubDir to SingletonContext and Context.

--- context.go
+++ context.go
@@ -2114,6 +2114,11 @@ func (c *Context) ModuleDir(logicModule Module) string {
 	return filepath.Dir(module.relBlueprintsFile)
 }
 
+func (c *Context) ModuleSubDir(logicModule Module) string {
+	module := c.moduleInfo[logicModule]
+	return module.variantName
+}
+
 func (c *Context) BlueprintFile(logicModule Module) string {
 	module := c.moduleInfo[logicModule]
 	return module.relBlueprintsFile
