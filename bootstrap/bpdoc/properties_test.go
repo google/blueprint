9810dcd4b90e84c96adcfcb7c222e51a136d20d4
Liz Kammer <eakammer@google.com>
2020-07-28 09:30:46 -0700

    Update bpdocs to filter nested properties by tags (#312)
    * Update bpdocs to filter nested properties by tag
    
    Prior to this change nested properties tagged with `blueprint:"mutated"`
    are erroneously included in documentation, this corrects that behavior
    and adds testing to verify.
    
    Tested: go test bpdocs tests
    Change-Id: I822c9a98276634d2f584d8709e83003824cdffd5

--- bootstrap/bpdoc/properties_test.go
+++ bootstrap/bpdoc/properties_test.go
@@ -28,11 +28,8 @@ func TestExcludeByTag(t *testing.T) {
 
 	ps.ExcludeByTag("tag1", "a")
 
-	expected := []string{"c"}
-	actual := []string{}
-	for _, p := range ps.Properties {
-		actual = append(actual, p.Name)
-	}
+	expected := []string{"c", "d", "g"}
+	actual := actualProperties(t, ps.Properties)
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("unexpected ExcludeByTag result, expected: %q, actual: %q", expected, actual)
 	}
@@ -47,12 +44,20 @@ func TestIncludeByTag(t *testing.T) {
 
 	ps.IncludeByTag("tag1", "c")
 
-	expected := []string{"b", "c"}
-	actual := []string{}
-	for _, p := range ps.Properties {
-		actual = append(actual, p.Name)
-	}
+	expected := []string{"b", "c", "d", "f", "g"}
+	actual := actualProperties(t, ps.Properties)
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("unexpected IncludeByTag result, expected: %q, actual: %q", expected, actual)
 	}
+}
+
+func actualProperties(t *testing.T, props []Property) []string {
+	t.Helper()
+
+	actual := []string{}
+	for _, p := range props {
+		actual = append(actual, p.Name)
+		actual = append(actual, actualProperties(t, p.Properties)...)
+	}
+	return actual
 }
